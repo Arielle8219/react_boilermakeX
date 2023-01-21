e6bcf5433386747aa1b97a2df1a9315e9395716b
Arielle Dong <dong344@purdue.edu>
2023-01-21 16:21:24 -0500

    login

--- StackNavigator.js
+++ StackNavigator.js
@@ -2,6 +2,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import React from 'react'
 import Homescreen from "./screens/Homescreen";
 import ChatScreen from "./screens/ChatScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -10,6 +11,7 @@ const StackNavigator = () => {
         <Stack.Navigator>
             <Stack.Screen name = "Home" component = {Homescreen} />
             <Stack.Screen name = "Chat" component = {ChatScreen} />
+            <Stack.Screen name = "Login" component = {LoginScreen} />
         </Stack.Navigator>
     );
 };
