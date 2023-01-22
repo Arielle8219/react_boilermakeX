744d29b2364a6c3c3f3b6d64ffb3f5828589a3a4
Arielle Dong <dong344@purdue.edu>
2023-01-22 01:57:40 -0500

    kjn

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { View, Text} from 'react-native';
-//import tw from "tailwind-rn";
+import React from "react";
+import { View, Text} from "react-native";
+import tw from "tailwind-rn";
 import { Foundation } from "@expo/vector-icons";
 import { Ionicons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/core";
@@ -13,8 +13,8 @@ const Header = ({title, callEnabled}) => {
     const navigation = useNavigation();
 
     return (
-        <View style={tw('p-2 flex-row items-center justify-between')}>
-            <View style={tw("flex felx-row items-center")} >
+        <View style={tw("p-2 flex-row items-center justify-between")}>
+            <View style={tw("flex flex-row items-center")} >
                 <TouchableOpacity onPress={() => navigation.goBack()} style  = {tw("p-2")}>
                     <Ionicons name= "chevron-back-outline" size={34} color="#FF5864" />
                 </TouchableOpacity>
